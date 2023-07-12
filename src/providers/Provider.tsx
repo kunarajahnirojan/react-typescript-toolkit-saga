13908faaa0ebdd3229da138de34fef848c69273c
kunarajahnirojan <kunarajahnirojan@gmail.com>
2023-07-12 20:41:39 +0530

    changes

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -3,7 +3,7 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'components/base';
 import { lang, name } from 'config';
-import store from 'store';
+// import store from 'store';
 import { Provider } from 'react-redux';
 export interface IAppProviderProps {
   children: React.ReactNode;
@@ -12,15 +12,15 @@ export interface IAppProviderProps {
 export default function AppProvider(props: IAppProviderProps) {
   return (
     <ErrorBoundary>
-      <Provider store={store}>
-        <HelmetProvider>
-          <Helmet>
-            <html lang={lang} />
-            <title>{name}</title>
-          </Helmet>
-          <BrowserRouter>{props.children}</BrowserRouter>
-        </HelmetProvider>
-      </Provider>
+      {/* <Provider store={store}> */}
+      <HelmetProvider>
+        <Helmet>
+          <html lang={lang} />
+          <title>{name}</title>
+        </Helmet>
+        <BrowserRouter>{props.children}</BrowserRouter>
+      </HelmetProvider>
+      {/* </Provider> */}
     </ErrorBoundary>
   );
 }
