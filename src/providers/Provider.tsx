a9c5fbc83d654c4d40a5b96776e2bfb1870374a9
Nirojan <kunarajahnirojan@gmail.com>
2023-08-17 10:21:25 +0530

    persist added

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-import { ErrorBoundary } from 'components/base';
-import { lang, name } from 'config';
-import store from 'store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ErrorBoundary, Loading } from 'components/base';
+import { lang, name } from 'config';
+import store, { persistor } from 'store';
+import animation from 'assets/animations/Loading/clock-loading.json';
+
 export interface IAppProviderProps {
   children: React.ReactNode;
 }
@@ -13,13 +16,18 @@ export default function AppProvider(props: IAppProviderProps) {
   return (
     <ErrorBoundary>
       <Provider store={store}>
-        <HelmetProvider>
-          <Helmet>
-            <html lang={lang} />
-            <title>{name}</title>
-          </Helmet>
-          <BrowserRouter>{props.children}</BrowserRouter>
-        </HelmetProvider>
+        <PersistGate
+          loading={<Loading animation={animation} />}
+          persistor={persistor}
+        >
+          <HelmetProvider>
+            <Helmet>
+              <html lang={lang} />
+              <title>{name}</title>
+            </Helmet>
+            <BrowserRouter>{props.children}</BrowserRouter>
+          </HelmetProvider>
+        </PersistGate>
       </Provider>
     </ErrorBoundary>
   );
