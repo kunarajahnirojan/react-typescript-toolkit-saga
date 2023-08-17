8aa738a17f70f9ca7f0cc327b347ed360bfa29d2
Nirojan <kunarajahnirojan@gmail.com>
2023-08-17 10:24:32 +0530

    persist added

--- src/persist/index.ts
+++ src/persist/index.ts
@@ -1,20 +1,6 @@
 import storage from 'redux-persist/lib/storage';
-import { encryptTransform } from 'redux-persist-transform-encrypt';
-import { secretKey } from 'config';
-
 const persistConfig = {
   key: 'root',
-  version: 1,
   storage,
-  whitelist: ['auth'],
-  transforms: [
-    encryptTransform({
-      secretKey,
-      onError: (error: Error) => {
-        return null;
-      },
-    }),
-  ],
 };
-
 export default persistConfig;
