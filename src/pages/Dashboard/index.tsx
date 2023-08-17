e69a48b1905545a64bed6700814c1399f032b48a
Nirojan <kunarajahnirojan@gmail.com>
2023-08-17 12:00:28 +0530

    changes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,50 +1,25 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loginRequest } from 'features/auth/authSlice';
-import { user as useSelectUser } from 'selectors/auth';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loadTitleRequest } from 'features/addons/addonSlice';
 
 const App = () => {
   const dispatch = useDispatch();
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const { loading, error } = useSelector((state: any) => state.auth);
-
-  const selectUser = useSelector(useSelectUser);
-
-  console.log('====================================');
-  console.log(selectUser);
-  console.log('====================================');
-
-  const handleLogin = () => {
-    dispatch(loginRequest({ username, password }));
-  };
+  useEffect(() => {
+    dispatch(loadTitleRequest());
+  }, [dispatch]);
 
   return (
     <div>
       <div>
-        <input
-          type="text"
-          placeholder="Email"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <input type="text" placeholder="Email" />
       </div>
       <div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <input type="password" placeholder="Password" />
       </div>
       <div>
-        <button onClick={handleLogin} disabled={loading}>
-          {loading ? 'Logging in...' : 'Login'}
-        </button>
+        <button>Logging in...</button>
       </div>
-      {error && <div>{error}</div>}
     </div>
   );
 };
