db210dbaf15d115403e40fa24fca50334a9a3c88
Nirojan <kunarajahnirojan@gmail.com>
2023-08-17 08:45:00 +0530

    Changes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { loginRequest } from 'features/auth/authSlice';
 
 const App = () => {
