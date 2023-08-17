8ce3378626cebbc9077f712ce6f03db5c5081bb7
Nirojan <kunarajahnirojan@gmail.com>
2023-08-17 08:53:39 +0530

    changes

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,12 +11,21 @@ import {
 } from 'redux-persist';
 import persistConfig from 'persist';
 import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 
 //
 import rootReducer from './reducer';
 import { rootSaga } from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
+
+//logging redux actions
+const loggerMiddleware = createLogger({
+  collapsed: true,
+  diff: true,
+  duration: true,
+  timestamp: false,
+});
 
 const store = configureStore({
   reducer: persistReducer(persistConfig, rootReducer),
@@ -26,7 +35,7 @@ const store = configureStore({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }).concat([sagaMiddleware]),
+    }).concat([sagaMiddleware, loggerMiddleware]),
 });
 sagaMiddleware.run(rootSaga);
 export type State = ReturnType<typeof store.getState>;
